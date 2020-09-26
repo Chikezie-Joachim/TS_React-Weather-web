d1c939b414a9ec86dd8a0b5d31db9e0f5c63ad1a
Chikezie-Joachim <Chukwudi2726.cj@gmail.com>
2020-09-25 21:38:21 -0700

    progree...

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,36 +1,39 @@
-import * as React from "react";
-import logo from "../logo.svg";
+import React, { FC } from "react";
 import "./App.scss";
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:3000/api.json",
-});
+const App: FC = () => {
+  return <div className="app"></div>;
+};
 
-class App extends React.Component{
-  state = {
-    flask: [],
-  };
-  constructor(appProps){
-    super(appProps);
-  }
+// const api = axios.create({
+//   baseURL: "http://localhost:3000/api.json",
+// });
 
-  render() {
-    return (
-      <div className="containex">
-        <div className="ent_bxc">
-          <div className="fogext">
-            <div className="mmg">
-              <img src="" alt="" />
-            </div>
-            <div className="fgn"> </div>
-            <div className="ctynt" data-stc="{}"></div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+// class App extends React.Component{
+//   state = {
+//     flask: [],
+//   };
+//   constructor(appProps){
+//     super(appProps);
+//   }
+
+//   render() {
+//     return (
+//       <div className="containex">
+//         <div className="ent_bxc">
+//           <div className="fogext">
+//             <div className="mmg">
+//               <img src="" alt="" />
+//             </div>
+//             <div className="fgn"> </div>
+//             <div className="ctynt" data-stc="{}"></div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 // const App: React.FC = () => {
 
 // };
