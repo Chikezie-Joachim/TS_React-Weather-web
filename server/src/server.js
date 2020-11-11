d4a8ed130c230dee5993aeaf241e1feebfebc628
Chikezie-Joachim <Chukwudi2726.cj@gmail.com>
2020-11-11 14:11:53 -0800

    init noeds srver

---
+++ server/src/server.js
@@ -0,0 +1,35 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const port = 3080;
+
+const api_key = process.env.api_key;
+app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "content-Type");
+  next();
+});
+
+app.post("/weather", (req, res) => {
+  const city = req.body.city;
+  console.log(req.body.city);
+  const uri = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+  axios({
+    url: uri,
+    responseType: "json",
+  }).then((data) => {
+    const d = data;
+    res.json(d.data);
+    res.status(500).json("oh noes!");
+    res.status(404).json("I dont have that");
+  });
+});
+
+app.listen(port, () => {
+  console.log("listening server at:" + port);
+});
