95e276ea4e00e8893ed1ebb66d074e079fb503dc
Chikezie-Joachim <Chukwudi2726.cj@gmail.com>
2020-11-12 04:49:18 -0800

    removed .tsx test components

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./app/App";
 import * as serviceWorker from "./serviceWorker";
-import Test from "./app/test";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    {/* <Test/> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
